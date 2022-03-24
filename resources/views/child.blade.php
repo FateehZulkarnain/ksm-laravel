f41909626eea550e756a687d230fac43bf7c1119
Fateeh Zulkarnain <fateehzulkarnain@gmail.com>
2022-03-24 10:30:59 +0800

    Navbar Lesson 1

---
+++ resources/views/child.blade.php
@@ -0,0 +1,7 @@
+    @extends('layouts.app')
+
+    {{-- Ini adalah extended layout daripada app.blade --}}
+
+    @section('content')
+        <p>This is my body content.</p>
+    @endsection
---
+++ resources/views/child1.blade.php
@@ -0,0 +1,7 @@
+    @extends('layouts.app')
+
+    {{-- Another xtended layout daripada app.blade --}}
+
+    @section('content')
+        <strong>This is my second body content.</strong>
+    @endsection
